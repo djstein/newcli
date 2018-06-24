19ac15374596d39dd6371527ab254f4958ba0d9f
Dylan Stein <djstein@ncsu.edu>
2018-06-24 13:48:25 -0400

    Add placeholder github push commands

--- newcli/core.py
+++ newcli/core.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from subprocess import call
 from findreplace.core import findreplace
 
 ROOT_DIR = os.path.dirname(__file__)
@@ -13,6 +14,7 @@ def setup_new_project(dest_dir=os.getcwd()):
     dest_dir = os.path.join(dest_dir, config.get('{{project}}'))
     copy_template(PROJECT_TEMPLATE_DIR, dest_dir)
     findreplace(base_dir=dest_dir, find_replace_dict=config)
+    create_github_repo(config)
 
 def prompt_config():
     config = {}
@@ -20,6 +22,23 @@ def prompt_config():
     config['{{name}}'] = input("Name: ")
     config['{{email}}'] = input("Email: ")
     config['{{github_user}}'] = input("GitHub Username: ")
-    config['{{github_repo}}'] = input("GitHub Repository: [https://github.com/{}/{}] ".format(config.get('{{github_user}}'), config.get('{{project}}')))
+
+    placeholder_github_repo = "https://github.com/{}/{}".format(config.get('{{github_user}}'), config.get('{{project}}'))
+    github_repo = input("GitHub Repository: [{}] ".format(placeholder_github_repo))
+    if not github_repo:
+        github_repo = placeholder_github_repo
+    config['{{github_repo}}'] = github_repo
+
     config['{{description}}'] = input("Description: ")
     return config
+
+def create_github_repo(config, push=True):
+    '''If in new project directory, begin clone and push to GitHub.'''
+    pass
+    # path = '.'
+    # call(["git", "init", path])
+    # call(["git", "-C", path, "add", "."])
+    # call(["git", "-C", path, "commit", "-m", "'Initial Commit - from newcli'"])
+    # call(["git", "-C", path, "remote", "add", "origin", repository])
+    # if push:
+    #     call(["git", "-C", path, "push", "-u", "origin", "master"])
