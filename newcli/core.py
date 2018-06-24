5a83b6885f312903bc661c6ddd9739b5caa61a70
Dylan Stein <djstein@ncsu.edu>
2018-06-24 15:12:34 -0400

    cookiecutter output dir

--- newcli/core.py
+++ newcli/core.py
@@ -4,17 +4,10 @@ from subprocess import call
 from cookiecutter.main import cookiecutter
 
 ROOT_DIR = os.path.dirname(__file__)
-PROJECT_TEMPLATE_DIR = os.path.join(ROOT_DIR, 'templates', '{{project}}')
-
-# def copy_template(source_dir, dest_dir):
-    # shutil.copytree(source_dir, dest_dir)
 
 def setup_new_project(dest_dir=os.getcwd()):
-    # dest_dir = os.path.join(dest_dir, config.get('{{project}}'))
-    # copy_template(PROJECT_TEMPLATE_DIR, dest_dir)
-
-    cookiecutter(ROOT_DIR)
-    create_github_repo(config)
+    cookiecutter(ROOT_DIR, output_dir=dest_dir)
+    # create_github_repo(config,
 
 def create_github_repo(config, push=True):
     '''If in new project directory, begin clone and push to GitHub.'''
