c2860d2ee41db60ae4ea0a8232607da5223dd66a
Dylan Stein <djstein@ncsu.edu>
2018-06-24 15:25:20 -0400

    use template on github

--- newcli/core.py
+++ newcli/core.py
@@ -3,10 +3,10 @@ import shutil
 from subprocess import call
 from cookiecutter.main import cookiecutter
 
-ROOT_DIR = os.path.dirname(__file__)
+template = "https://github.com/djstein/cookiecutter-newcli.git"
 
 def setup_new_project(dest_dir=os.getcwd()):
-    cookiecutter(ROOT_DIR, output_dir=dest_dir)
+    cookiecutter(template, output_dir=dest_dir)
     # create_github_repo(config,
 
 def create_github_repo(config, push=True):
